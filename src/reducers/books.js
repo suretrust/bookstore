0a2fc0e7ed80c42f30566c7271269eafb4282fc4
Saheed <sdrfngg640@gmail.com>
2019-11-18 17:34:34 +0100

    Add create and remove book return values

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,12 +1,20 @@
-const reducer = (state = [], action) => {
+import getId from '../utilities/getId';
+
+const books = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
-      break;
+      return [
+        ...state,
+        { id: getId(), title: action.title, category: action.category }
+      ];
+
     case 'REMOVE_BOOK':
-      break;
+      return state.filter(book => book.id !== action.id);
 
     default:
       break;
   }
   return state;
 };
+
+export default books;
