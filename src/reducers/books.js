aa8ee3aaf77bcd0c53830df213e9b693a0db2012
Saheed <sdrfngg640@gmail.com>
2019-11-18 15:33:40 +0100

    Add reducer function

---
+++ src/reducers/books.js
@@ -0,0 +1,11 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CREATE_BOOK':
+      break;
+    case 'REMOVE_BOOK':
+      break;
+
+    default:
+      return state;
+  }
+};
