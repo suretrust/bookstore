b12154f1e0ee16c5d82c90019d2410e39291272d
Saheed <sdrfngg640@gmail.com>
2019-11-18 15:33:25 +0100

    Map books to return book with id, title, and category

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
 
+const mapStateToProps = state => ({ books: state.books });
 
-const BooksList = ({books}) => {
+const BooksList = ({ books }) => {
+  return (
+    <div>
+      <h1>BooksList</h1>
+      <table>
+        {books.map(book => (
+          <Book
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            category={book.category}
+          />
+        ))}
+      </table>
+    </div>
+  );
+};
 
-    const { bookID, title, category } = books
-
-    return (
-        <div>
-            <h1>BooksList</h1>
-            <table>
-                <tr>
-                    <td>Book ID: { bookID }</td>
-                    <td>title: { title }</td>
-                    <td>category: {category} </td>
-                </tr>
-            </table>
-        </div>
-    );
-}
-
-export default BooksList
+export default connect(mapStateToProps)(BooksList);
