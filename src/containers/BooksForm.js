224acb91643a865c1f9d572d3827b6587a6ac116
bellom <bellomsean@gmail.com>
2019-11-19 14:45:38 +0100

    added hanldeChange func

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,30 +1,47 @@
 import React from 'react';
 
-const BooksForm = () => {
-  const categories = [
-    'Action',
-    'Biography',
-    'History',
-    'Horror',
-    'Kids',
-    'Learning',
-    'Sci-Fi'
-  ];
+class BooksForm extends React.Component {
+  state = {
+    title: '',
+    category: ''
+  }
 
-  return (
-    <div>
-      <form>
-        <input type="text" placeholder="Book Title" />
-        <select name="" id="">
-          <option value="">Choose a category</option>
-          {categories.map(category => (
-            <option key={categories.indexOf(category)} value={category}>{category}</option>
-          ))}
-        </select>
-        <input type="submit" value="Add a Book" />
-      </form>
-    </div>
-  );
-};
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value 
+    })
+  };
+
+  
+
+  render() {
+
+    const categories = [
+      'Action',
+      'Biography',
+      'History',
+      'Horror',
+      'Kids',
+      'Learning',
+      'Sci-Fi'
+    ];
+  
+    return (
+      <div>
+        <form onChange={this.handleChange}>
+          <input type="text" name='title' value={this.state.title} placeholder="Book Title"/>
+          <select name="category" value={this.state.category} id="" >
+            <option value="">Choose a category</option>
+            {categories.map(category => (
+              <option key={categories.indexOf(category)} value={category}>{category}</option>
+            ))}
+          </select>
+          <input type="submit" value="Add a Book" />
+        </form>
+        <p>title: {this.state.title} || category: {this.state.category}</p>
+      </div>
+    );
+  }
+ };
 
 export default BooksForm;
