3f18536eca0b5bddc0d330a961ed1bd5a7f5aec5
Saheed <sdrfngg640@gmail.com>
2019-11-21 11:01:04 +0100

    Import removeBook and add prop type validation

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
+import { removeBook } from '../actions';
 
 const mapStateToProps = state => ({ books: state.books });
 
-const BooksList = ({ books }) => {
+const BooksList = ({ books, removeBook }) => {
   return (
     <div>
       <h1>BooksList</h1>
       <table>
         <tbody>
           {books.map(book => (
-            <Book key={book.id} book={book} />
+            <Book key={book.id} book={book} removeBook={removeBook} />
           ))}
         </tbody>
       </table>
@@ -27,7 +28,8 @@ BooksList.propTypes = {
       title: PropTypes.string.isRequired,
       category: PropTypes.string.isRequired
     }).isRequired
-  ).isRequired
+  ).isRequired,
+  removeBook: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, { removeBook })(BooksList);
