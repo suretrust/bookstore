ddc8d3584859e611bc623590e4b53eb8c406a753
Saheed <sdrfngg640@gmail.com>
2019-11-21 18:34:39 +0100

    Pass down handle change as props

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import categories from '../utilities/categoriesgit'
+import React from 'react';
+import categories from '../utilities/categories';
 
-const CategoryFilter = () => {
-
+const CategoryFilter = ({ handleChange }) => {
   return (
     <div>
       <span>Filter By Categotries: </span>
-      <select>
-          {categories.map(e => (<option>{e}</option>))}
+      <select onChange={handleChange}>
+        <option value="All">All</option>
+        {categories.map(e => (
+          <option key={e} value={e}>
+            {e}
+          </option>
+        ))}
       </select>
     </div>
   );
-}
+};
 
 export default CategoryFilter;
